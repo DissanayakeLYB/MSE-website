53e6940a7fd3d0a480aed04d73b8f73eb9278275
DissanayakeLYB <lasithdissanayake99@gmail.com>
2025-01-18 20:46:56 +0530

    updated header

---
+++ MSE/src/App.jsx
@@ -0,0 +1,16 @@
+import Header from './Header.jsx';
+import Footer from './Footer.jsx';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
---
+++ MSE/src/Footer.jsx
@@ -0,0 +1,10 @@
+
+function Footer(){
+    return(
+        <footer>
+            <p>&copy; {new Date().getFullYear()} - Materials Science and Engineering</p>
+        </footer>
+    );
+}
+
+export default Footer;
